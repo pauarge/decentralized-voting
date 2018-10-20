3ae01f86798a6961d915b4780db4e245e3b13334
Pau Argelaguet <pau@outlook.com>
2018-10-20 16:20:36 +0200

    Better error handling on backend

--- node/main.py
+++ node/main.py
@@ -1,5 +1,3 @@
-import hashlib
-
 from flask import Flask, request, jsonify, render_template, send_file
 from flask_cors import CORS
 from flask_qrcode import QRcode
@@ -8,6 +6,7 @@ import string
 import time
 import copy
 import json
+import hashlib
 
 from config import SALT_QR
 from send_email import send_register_email
@@ -20,29 +19,35 @@ qrcode = QRcode(app)
 
 app.blocks = []
 app.model = Model()
-known_hosts = []
-
-
-# scheduler.start()
+app.known_hosts = []
 
 
 @app.route("/election", methods=['POST'])
 def election():
     data = request.get_json()
-    # if not app.current_election:
-    elec = data
-    elec['id'] = ''.join(
-        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
-    elec['voted'] = []
-    elec['options'] = [{'index': i, 'name': x, 'votes': 0} for i, x in enumerate(data.get('options'))]
-    elec['pointer'] = 0
-    elec['hash'] = ''
 
-    app.blocks.append(elec)
-    app.model.save(app.blocks)
-    broadcast_blocks(app.blocks, known_hosts)
-    send_register_email(elec, request.headers.get('host'))
-    return jsonify(app.blocks)
+    if data and 'users' in data and 'name' in data and 'description' in data and 'expiration' in data \
+            and 'options' in data:
+        if not app.blocks:
+            elec = data
+            elec['id'] = ''.join(
+                random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(32))
+            elec['voted'] = []
+            elec['options'] = [{'index': i, 'name': x, 'votes': 0} for i, x in enumerate(data.get('options'))]
+            elec['pointer'] = 0
+            elec['hash'] = ''
+
+            app.blocks.append(elec)
+            app.model.save(app.blocks)
+            broadcast_blocks(app.blocks, app.known_hosts)
+            send_register_email(elec, request.headers.get('host'))
+            return jsonify(app.blocks)
+        elif app.blocks[-1].get('expire') < time.time():
+            return jsonify({'error': 'there was an election but it expired, check results'})
+        else:
+            return jsonify({'error': 'there is an election ongoing'})
+
+    return jsonify({'error': 'invalid parameters'}), 400
 
 
 @app.route("/verify", methods=['GET'])
@@ -51,22 +56,26 @@ def verify():
 
 
 @app.route('/qrcode', methods=['POST'])
-def get_qrcode():
-    email = request.form.get('email')
-    id = request.form.get('id')
-    token = request.form.get('token')
-    user = {
-        'email': email,
-        'id': id
-    }
+def qrcode():
+    if 'email' in request.form and 'id' in request.form and 'token' in request.form:
+        email = request.form.get('email')
+        id = request.form.get('id')
+        token = request.form.get('token')
+        user = {
+            'email': email,
+            'id': id
+        }
 
-    sha = hashlib.sha512()
-    sha.update("{}{}{}{}".format(SALT_QR, email, id, app.blocks[-1]['id']).encode('utf-8'))
+        if len(app.blocks) < 1:
+            return jsonify({'error': 'no ongoing election'}), 400
 
-    if generate_token(app.blocks[-1], user) == token:
-        return send_file(qrcode(sha.hexdigest(), mode='raw'), mimetype='image/png')
-    else:
-        return 'incorrect parameters'
+        sha = hashlib.sha512()
+        sha.update("{}{}{}{}".format(SALT_QR, email, id, app.blocks[-1]['id']).encode('utf-8'))
+
+        if generate_token(app.blocks[-1], user) == token:
+            return send_file(qrcode(sha.hexdigest(), mode='raw'), mimetype='image/png')
+
+    return jsonify({'error': 'invalid parameters'}), 400
 
 
 @app.route('/options', methods=['POST'])
@@ -94,7 +103,7 @@ def vote():
         if result:
             app.blocks.append(new_block)
             app.model.save(app.blocks)
-            broadcast_blocks(app.blocks, known_hosts)
+            broadcast_blocks(app.blocks, app.known_hosts)
             return jsonify({'blocks': app.blocks, 'verification': result})
 
     return jsonify({'error': 'vote not registered'})
@@ -121,4 +130,4 @@ def update():
     if local_pointer < data['pointer']:
         app.current_election = data
     else:
-        broadcast_blocks(app.blocks, known_hosts)
+        broadcast_blocks(app.blocks, app.known_hosts)
