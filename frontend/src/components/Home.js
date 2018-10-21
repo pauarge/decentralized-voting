975c7694905c8665e6d1e29626d41c02eba8a39a
Victor Ramirez <victor.ramirez02@estudiant.upf.edu>
2018-10-21 08:06:42 +0200

    sweet landingpage alabama

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,21 +3,31 @@ import Box from "./UI/Containers";
 
 export default class Home extends Component {
     render() {
+        const isElectionActive = this.props.name != '';
+        let flexLeft;
+
+        console.log(isElectionActive);
+        
+        if (isElectionActive) {
+            flexLeft = <div className="Flex__Half Flex__Grey Flex__Separator">
+                            <h2 className="Page__Halftitle">{this.props.name}</h2>
+                            <p className="Page__Text">Voting process for {this.props.name} is currently open. Authenticate here to see the options available and cast your vote.</p>
+
+                            <p className="Page__Text">
+                                <button className="Button Button__Green">Vote now</button>
+                            </p>
+                        </div>;
+        } else {
+            flexLeft = '';
+        }
         return(
             <>
             <h3 className="Page__Pretitle">Decentralized Voting System</h3>
             <Box nopad>
                 <div className="Flex__Double">
-                    <div className="Flex__Half Flex__Grey Flex__Separator">
-                        <h2 classNames="Page__Halftitle">{this.props.name}</h2>
-                        <p className="Page__Text">Voting process for {this.props.name} is currently open. Authenticate here to see the options available and cast your vote.</p>
-
-                        <p className="Page__Text">
-                            <button className="Button Button__Green">Vote now</button>
-                        </p>
-                    </div>
+                    {flexLeft}
                     <div className="Flex__Half">
-                        <h2 classNames="Page__Halftitle">Register a new event</h2>
+                        <h2 className="Page__Halftitle">Register a new event</h2>
                         <p className="Page__Text">Use the following form to sign up for a new electoral issue or voting event.</p>
                         <p className="Page__Text">
                             <button className="Button Button__Secondary">Sign up</button>
