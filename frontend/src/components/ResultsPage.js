1816c3b7c660caeb1a6f44af7d1d8706fd411e9e
Boris Barath <boris.barath16@imperial.ac.uk>
2018-10-21 08:19:06 +0200

    Working results page

--- frontend/src/components/ResultsPage.js
+++ frontend/src/components/ResultsPage.js
@@ -3,15 +3,43 @@ import React, { Component } from "react";
 import Footer from "./Footer/FooterContainer";
 import "../App.scss";
 import "./ManagedElections.scss";
+import { SERVER_PATH } from "../config";
 
 export default class ResultsPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: [],
+      votes: []
+    };
+
+    this.fetchCurrentElection();
+  }
+
+  async fetchCurrentElection() {
+    fetch(`${SERVER_PATH}/results`, {
+      method: "POST"
+    }).then(response => {
+      response.json().then(data => {
+        console.log(data);
+        this.setState({ options: data.options, votes: data.results });
+      });
+    });
+  }
+
   render() {
     return (
       <>
         <h1 className="Page__Title">The voting has ended.</h1>
-        <p className="Page__Lead">Option 1: 35 votes.</p>
-        <p className="Page__Lead">Option 2: 72 votes.</p>
-        <p className="Page__Lead">Option 3: 98 votes.</p>
+        {this.state.options.map(option => (
+          <p className="Page__Lead">
+            {option.name} :{" "}
+            {this.state.votes[option.index.toString()]
+              ? this.state.votes[option.index.toString()]
+              : 0}
+          </p>
+        ))}
         {/* <Footer from="/options" fromLabel="Try again" /> */}
       </>
     );
